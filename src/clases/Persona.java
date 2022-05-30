89595c5ed15438e76f20686683725a548a7fcd92
rafamb <rmonber2302@g.educaand.es>
2022-05-30 11:52:17 +0200

    Creo clase persona con sus propiedades

---
+++ src/clases/Persona.java
@@ -0,0 +1,14 @@
+package clases;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Persona {
+	private String dni;
+	private String nombre;
+	private String ap1;
+	private String ap2;
+	private Calendar fecha_nac;
+	private Direccion direccion;
+}
