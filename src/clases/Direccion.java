279b8ef9a65d20903204a8c53c2e1f2d8e0dda17
rafamb <rmonber2302@g.educaand.es>
2022-05-30 12:06:06 +0200

    Creo getter and Setters, metodo constructor y metodo toString

--- src/clases/Direccion.java
+++ src/clases/Direccion.java
@@ -4,4 +4,50 @@ public class Direccion {
 	private String nombre_via;
 	private int num_via;
 	private int cod_postal;
+	
+	//Setter and getters
+	
+	public String getNombre_via() {
+		return nombre_via;
+	}
+	public void setNombre_via(String nombre_via) {
+		this.nombre_via = nombre_via;
+	}
+	public int getNum_via() {
+		return num_via;
+	}
+	public void setNum_via(int num_via) {
+		this.num_via = num_via;
+	}
+	public int getCod_postal() {
+		return cod_postal;
+	}
+	public void setCod_postal(int cod_postal) {
+		this.cod_postal = cod_postal;
+	}
+	
+	
+	//Metodo Constructor
+	
+	public Direccion(String nombre_via, int num_via, int cod_postal) {
+		super();
+		this.nombre_via = nombre_via;
+		this.num_via = num_via;
+		this.cod_postal = cod_postal;
+	}
+	
+	//Metodo toString
+	
+	@Override
+	public String toString() {
+		return "Direccion [nombre_via=" + nombre_via + ", num_via=" + num_via + ", cod_postal=" + cod_postal + "]";
+	}
+	
+	
+	
+	
+	
 }
+
+
+
