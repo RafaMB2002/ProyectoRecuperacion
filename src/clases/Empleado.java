7d54e80e94c8c866b098e15715079a3597d5a8f2
rafamb <rmonber2302@g.educaand.es>
2022-05-30 11:58:30 +0200

    Creo clase Empleado con sus propiedades que hereda de Persona

---
+++ src/clases/Empleado.java
@@ -0,0 +1,9 @@
+package clases;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Empleado extends Persona{
+	private Calendar fecha_alta;
+	private Oficina oficina;
+}
