882866f2922aea889b6a17ffebbb0ac4eb4d5c9f
rafamb <rmonber2302@g.educaand.es>
2022-05-31 09:34:13 +0200

    arreglo tipo de dato de fecha

--- src/clases/Cliente.java
+++ src/clases/Cliente.java
@@ -1,10 +1,11 @@
 package clases;
 
+import java.sql.Date;
 import java.util.Calendar;
 
 public class Cliente extends Persona{
 
-	public Cliente(String dni, String nombre, String ap1, String ap2, Calendar fecha_nac, Direccion direccion) {
+	public Cliente(String dni, String nombre, String ap1, String ap2, Date fecha_nac, Direccion direccion) {
 		super(dni, nombre, ap1, ap2, fecha_nac, direccion);
 		// TODO Auto-generated constructor stub
 	}
