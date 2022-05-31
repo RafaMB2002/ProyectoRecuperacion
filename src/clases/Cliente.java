0b8da5e9b30a77d50cd38f33acc30ba0aa9d49cf
rafamb <rmonber2302@g.educaand.es>
2022-05-31 08:29:42 +0200

    commit cliente

--- src/clases/Cliente.java
+++ src/clases/Cliente.java
@@ -1,5 +1,12 @@
 package clases;
 
+import java.util.Calendar;
+
 public class Cliente extends Persona{
+
+	public Cliente(String dni, String nombre, String ap1, String ap2, Calendar fecha_nac, Direccion direccion) {
+		super(dni, nombre, ap1, ap2, fecha_nac, direccion);
+		// TODO Auto-generated constructor stub
+	}
 
 }
