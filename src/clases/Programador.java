fb9f5f1649d3f6396f75290b6d4535502026a994
rafamb <rmonber2302@g.educaand.es>
2022-06-01 17:49:41 +0200

    Metodo nomina para programador

--- src/clases/Programador.java
+++ src/clases/Programador.java
@@ -20,6 +20,29 @@ public class Programador extends Empleado{
 		super(dni, nombre, ap1, ap2, fecha_nac, direccion, fecha_alta, oficina);
 		this.tecnologia = tecnologia;
 	}
+
+	@Override
+	public double nomina(int mes) {
+		double nomina=0;
+		int sueldoPorDia=0;
+		int sueldoPorTecnologia=0;
+		final int sueldoBase = 500;
+		final int sueldoAntiguedad = antiguedad()*25;
+		
+		if(mes == 2) {
+			sueldoPorDia = 5*28;
+		}
+		if(mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+			sueldoPorDia = 5*30;
+		}
+		if(mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+			sueldoPorDia = 5*31;
+		}
+		sueldoPorTecnologia = tecnologia.size()*20;
+		nomina = sueldoBase+sueldoAntiguedad+sueldoPorDia+sueldoPorTecnologia;
+		
+		return nomina;
+	}
 	
 	
 }
