d543b8f26508ee01084f0839f8a5a7c6b5ab1e12
rafamb <rmonber2302@g.educaand.es>
2022-06-01 18:18:15 +0200

    Creo clase conexion

---
+++ src/accesoADatos/PruebaConexion.java
@@ -0,0 +1,57 @@
+package accesoADatos;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.awt.event.ActionEvent;
+
+public class PruebaConexion extends JFrame {
+	public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static final String usuario = "System";
+	public static final String password = "1477";
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PruebaConexion frame = new PruebaConexion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PruebaConexion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("Conectar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				conexionBD con = new conexionBD();
+				Connection conexion = con.getConnection();
+			}
+		});
+		contentPane.add(btnNewButton, BorderLayout.CENTER);
+	}
+
+}
--- src/accesoADatos/conexionBD.java
+++ src/accesoADatos/conexionBD.java
@@ -1,5 +1,29 @@
 package accesoADatos;
 
-public class conexionBD {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+import javax.swing.JOptionPane;
+
+public class conexionBD {
+	//Declaracion de atributos
+		public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+		public static final String usuario = "System";
+		public static final String password = "1477";
+		/**
+		 * Metodo para conectar a base de datos
+		 * @return devuelve la conexion
+		 */
+		public Connection getConnection() {
+			Connection conexion=null;
+			
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conexion = (Connection) DriverManager.getConnection(URL, usuario, password);
+				//JOptionPane.showMessageDialog(null, "Conexion correcta!");
+			}catch(Exception ex) {
+				System.out.println("Error, "+ex);
+			}
+			return conexion;
+		}
 }
